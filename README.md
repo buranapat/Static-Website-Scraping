cf4ffb2e213e69d39fcdd3b2c7b75b849ad6f216
buranapat <buranaphatt.as@gmail.com>
2022-08-19 22:50:53 +0700

    first project on github

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Static-Website-Scraping
-Static Web Scraping from Intermediate Data Engineering by Isarapong Eksinchol, PhD (GBDi)
+### Static Web Scraping 
+### from Intermediate Data Engineering 
+### by P' Isarapong Eksinchol, PhD (GBDi)
+---
+### Scrape company news from SET (The Stock Exchange of Thailand) using `requests` and `BeautifulSoup`.
+
+## Workflow
+1. Import `requests` and `BeautifulSoup` library.
+2. Get `HTML` code from company news webpage and turn in to `txt`
+3. Inspect `HTML` code from that webpage to find the part of the company news
+4. Turn `txt` to `BeautifulSoup` object to distill data and get the company news
+5. Transform company news to `pandas DataFrame` to export in form of Excel
+
---
+++ main.py
@@ -0,0 +1,58 @@
+def get_news(symbol):
+    import requests
+    from bs4 import BeautifulSoup
+
+    url = f"https://classic.set.or.th/set/companynews.do?symbol={symbol}&language=th&country=TH" 
+    source = requests.get(url).text
+
+    soup = BeautifulSoup(source,'lxml')
+
+    maincontent = soup.find(
+        "div",
+        id = "maincontent"
+    )
+
+    div_row = maincontent.find(
+        "div",
+         class_="row"
+    )
+
+    body = div_row.findChildren(
+        "div",
+        class_ = "table-responsive",
+        recursive = False
+    )[-1].find(
+          "table",
+           class_ = "table table-hover table-info-wrap"
+    ).findChildren(
+          "tbody",
+          recursive = False
+    )[0] 
+
+    rows = body.findChildren(
+          "tr",
+          recursive = False
+    )
+
+    lsls=[]
+    for row in rows:
+        cells=row.findChildren(
+              "td",
+               recursive = False
+        )
+        ls =[]
+
+        for cell in cells:
+            ls += [cell.text.replace('\n','').replace('\r','').strip()]
+        lsls += [ls]
+
+    import pandas as pd
+    df = pd.DataFrame(
+          lsls,
+        columns=['date','','source','detail','']
+    )
+    df.to_excel(f'companynews_{symbol}.xlsx')
+
+stocks = ['advanc','aot','ptt','scb','true'] # example stocks
+for stock in stocks:
+    get_news(stock.upper())
